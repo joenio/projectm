39b99c71bae25d068756ec2eb31090d692c8c1be
Joenio Costa <joenio@joenio.me>
2020-01-08 22:51:51 -0300

    name application as projectM

--- src/projectM-gtk/projectM-gtk.c
+++ src/projectM-gtk/projectM-gtk.c
@@ -3,7 +3,7 @@
 static void activate (GtkApplication* app, gpointer user_data) {
   GtkWidget *window;
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Window");
+  gtk_window_set_title(GTK_WINDOW(window), "projectM");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
   gtk_widget_show_all(window);
 }
@@ -11,7 +11,7 @@ static void activate (GtkApplication* app, gpointer user_data) {
 int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new("net.sourceforge.projectM-gtk", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
